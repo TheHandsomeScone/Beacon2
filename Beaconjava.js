d9dee0f81e264914daed205574a3d4cd4b24fff8
Zarram <thecakeisalie2801@gmail.com>
2020-03-05 12:22:14 +0100

    comment update

--- Beaconjava.js
+++ Beaconjava.js
@@ -1,7 +1,3 @@
-
-
-
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -16,12 +12,14 @@ window.onclick = function(event) {
   }
 }
 
+//Ændre billede
 function changeImage(){
 var   img = document.getElementById("imagekort");
 img.src="https://i.imgur.com/o5M7ceG.png";
 return false;
 }
 
+//Ænder billede
 function changeImage2(){
 var img = document.getElementById("imagekort");
 img.src="https://www.edrawsoft.com/template/mall-floor-plan.png?fbclid=IwAR2Yc3ai96sVPDALHATM0YXn0nWM6UTHNd3jqJON5OrNGuBTTMYH5-6C_UU";
